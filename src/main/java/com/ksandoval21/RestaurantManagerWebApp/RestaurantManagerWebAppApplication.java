7dc24c87ee4898730bb56aa225a4d65422ec164b
Kenia Sandoval <72216996+ksandoval21@users.noreply.github.com>
2021-06-07 16:07:48 -0500

    progress on landing page template

--- src/main/java/com/ksandoval21/RestaurantManagerWebApp/RestaurantManagerWebAppApplication.java
+++ src/main/java/com/ksandoval21/RestaurantManagerWebApp/RestaurantManagerWebAppApplication.java
@@ -2,6 +2,10 @@ package com.ksandoval21.RestaurantManagerWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RestaurantManagerWebAppApplication {
@@ -9,5 +13,11 @@ public class RestaurantManagerWebAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantManagerWebAppApplication.class, args);
 	}
-
+	@Controller
+	public class WelcomePage {
+		@GetMapping("/")
+		public String welcome(){
+			return "welcome";
+		}
+	}
 }
